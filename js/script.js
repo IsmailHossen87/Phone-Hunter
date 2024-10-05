68af4b92cc45d58a6f27a9315ac501e330627be2
Ismail <ismailhosen8757@gmail.com>
2024-10-05 12:55:42 +0600

    onclick okh

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
-const handleSearch = () =>{
-document.getElementById('spinner').style.display='block'
-const searchBox= document.getElementById('search-box').value;
+const handleSearch = () => {
+  document.getElementById("spinner").style.display = "block";
+  const searchBox = document.getElementById("search-box").value;
 
-   setTimeout(function () {
-    loadPhone(false,searchBox)
-   }, 2000);
-}
+  setTimeout(function () {
+    loadPhone(false, searchBox);
+  }, 2000);
+};
 
-const loadPhone = async(status ,searchBox) =>{
-    document.getElementById('spinner').style.display='none'
-    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchBox?searchBox:'iphone'}`)
-    const data = await response.json();
-    console.log(data)
-    if(status){
-        displayAllPhone(data.data)
-    }else{
-        displayAllPhone(data.data.slice(0,6))
-    }
-}
-const displayAllPhone = (phones) =>{
-   const phonesContainer = document.getElementById('phones-container')
-//    its a impotant section
-   document.getElementById('phones-container').innerHTML =''
-phones.forEach(phone => {
-    const {brand ,image,slug}= phone
-    console.log(phone)
-    const div = document.createElement('div')
+const loadPhone = async (status, searchBox) => {
+  document.getElementById("spinner").style.display = "none";
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${
+      searchBox ? searchBox : "iphone"
+    }`
+  );
+  const data = await response.json();
+//   console.log(data);
+  if (status) {
+    displayAllPhone(data.data);
+  } else {
+    displayAllPhone(data.data.slice(0, 6));
+  }
+};
+const displayAllPhone = (phones) => {
+  const phonesContainer = document.getElementById("phones-container");
+  //    its a impotant section
+  document.getElementById("phones-container").innerHTML = "";
+  phones.forEach((phone) => {
+    const { brand, image, slug } = phone;
+    // console.log(phone);
+    const div = document.createElement("div");
     div.innerHTML = `
     <div class="card card-compact mt-5 space-y-4 shadow-xl">
         <figure>
@@ -40,36 +44,40 @@ phones.forEach(phone => {
         </div>
         </div>
     
-    `
-    phonesContainer.append(div)
-});
+    `;
+    phonesContainer.append(div);
+  });
+};
+async function phoneDetails(slugs) {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${slugs}`
+  );
+  const data = await res.json();
+  const { brand,image,name,slug,releaseDate
+  } = data.data
 
- async function phoneDetails(slug) {
-    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
-    const data = await res.json()
-    console.log(data)
 
-
-
-    // const modalContainer = document.createElement('div')
-    // modalContainer.innerHTML =`
-    // <!-- Open the modal using ID.showModal() method -->
-    //     <button class="btn" onclick="my_modal_2.showModal()">open modal</button>
-    //     <dialog id="my_modal_2" class="modal">
-    //     <div class="modal-box">
-    //         <h3 class="text-lg font-bold">Hello!</h3>
-    //         <p class="py-4">Press ESC key or click outside to close</p>
-    //     </div>
-    //     <form method="dialog" class="modal-backdrop">
-    //         <button>close</button>
-    //     </form>
-    //     </dialog>
-    // `
+  const modalContainer = document.getElementById('modal-container')
+  modalContainer.innerHTML =`
+    <!-- Open the modal using ID.showModal() method -->
+    <dialog id="my_modal_1" class="modal">
+    <div class="modal-box">
+        <h3 class="text-lg font-bold">${brand}</h3>
+        <p class="py-4">${name}</p>
+        <p class="py-4">${releaseDate}</p>
+        <div class="modal-action">
+        <form method="dialog">
+            <!-- if there is a button in form, it will close the modal -->
+            <button class="btn btn-primary">Close</button>
+        </form>
+        </div>
+    </div>
+    </dialog>
+  `
+  my_modal_1.showModal()
 }
 
-
-}
-const handleShowAll = ()=> {
-    loadPhone(true)
-}
-loadPhone(false)
+const handleShowAll = () => {
+  loadPhone(true);
+};
+loadPhone(false);
